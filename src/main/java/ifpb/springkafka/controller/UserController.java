2048b6f8613156add886f3d92c47ffb3b60abebc
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-08-01 14:12:27 -0300

    Ajustes nas rotas de usuário

--- src/main/java/ifpb/springkafka/controller/UserController.java
+++ src/main/java/ifpb/springkafka/controller/UserController.java
@@ -5,6 +5,7 @@ import ifpb.springkafka.model.User;
 import ifpb.springkafka.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,14 +20,30 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserDto> create(@RequestBody @Valid UserDto userDto) {
+        if (userRepository.findByEmail(userDto.email()) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
         User user = new User();
         user.setName(userDto.name());
         user.setEmail(userDto.email());
-        userRepository.save(user);
+        User newUser = userRepository.save(user);
+
+        UserDto responseDto = new UserDto(newUser.getName(), newUser.getEmail());
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getById(@PathVariable Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        UserDto userDto = new UserDto(user.getName(), user.getEmail());
         return ResponseEntity.ok(userDto);
     }
 
-    @GetMapping("/find")
+    @GetMapping("/by-email")
     public ResponseEntity<UserDto> get(@RequestParam String email) {
         User user = userRepository.findByEmail(email);
         if (user == null) {
